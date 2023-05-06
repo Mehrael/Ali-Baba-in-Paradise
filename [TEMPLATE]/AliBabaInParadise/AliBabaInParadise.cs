93d9041512437d2f92c8ccd4b73ab097616eea4b
Mehrael <mehraelashraf20@gmail.com>
2023-05-06 13:57:48 +0300

    initialize project

---
+++ [TEMPLATE]/AliBabaInParadise/AliBabaInParadise.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem
+{
+    // *****************************************
+    // DON'T CHANGE CLASS OR FUNCTION NAME
+    // YOU CAN ADD FUNCTIONS IF YOU NEED TO
+    // *****************************************
+    public static class AliBabaInParadise
+    {
+        #region YOUR CODE IS HERE
+        #region FUNCTION#1: Calculate the Value
+        //Your Code is Here:
+        //==================
+        /// <summary>
+        /// Given the Camels maximum load and N items, each with its weight and profit 
+        /// Calculate the max total profit that can be carried within the given camels' load
+        /// </summary>
+        /// <param name="camelsLoad">max load that can be carried by camels</param>
+        /// <param name="itemsCount">number of items</param>
+        /// <param name="weights">weight of each item</param>
+        /// <param name="profits">profit of each item</param>
+        /// <returns>Max total profit</returns>
+        static public int SolveValue(int camelsLoad, int itemsCount, int[] weights, int[] profits)
+        {
+            //REMOVE THIS LINE BEFORE START CODING
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region FUNCTION#2: Construct the Solution
+        //Your Code is Here:
+        //==================
+        /// <returns>Tuple array of the selected items to get MAX profit (stored in Tuple.Item1) together with the number of instances taken from each item (stored in Tuple.Item2)
+        /// OR NULL if no items can be selected</returns>
+        static public Tuple<int, int>[] ConstructSolution(int camelsLoad, int itemsCount, int[] weights, int[] profits)
+        {
+            //REMOVE THIS LINE BEFORE START CODING
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #endregion
+    }
+}
