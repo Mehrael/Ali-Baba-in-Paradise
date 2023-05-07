87066e3eec92481f57654ea4823cc960f20b2460
Mehrael <mehraelashraf20@gmail.com>
2023-05-07 20:18:38 +0300

    First Function

--- [TEMPLATE]/AliBabaInParadise/AliBabaInParadise.cs
+++ [TEMPLATE]/AliBabaInParadise/AliBabaInParadise.cs
@@ -25,10 +25,21 @@ namespace Problem
         /// <param name="weights">weight of each item</param>
         /// <param name="profits">profit of each item</param>
         /// <returns>Max total profit</returns>
-        static public int SolveValue(int camelsLoad, int itemsCount, int[] weights, int[] profits)
+        public static int SolveValue(int camelsLoad, int itemsCount, int[] weights, int[] profits)
         {
             //REMOVE THIS LINE BEFORE START CODING
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            int[,] ExtraStorage = new int[itemsCount + 1, camelsLoad + 1];
+
+            for (int i = 1; i <= itemsCount; i++)
+                for (int j = 1; j <= camelsLoad; j++)
+                {
+                    ExtraStorage[i, j] = ExtraStorage[i - 1, j];
+                    if (j >= weights[i - 1])
+                        ExtraStorage[i, j] = Math.Max(ExtraStorage[i, j], ExtraStorage[i, j - weights[i - 1]] + profits[i - 1]);
+                }
+
+            return ExtraStorage[itemsCount, camelsLoad];
         }
         #endregion
 
@@ -37,7 +48,7 @@ namespace Problem
         //==================
         /// <returns>Tuple array of the selected items to get MAX profit (stored in Tuple.Item1) together with the number of instances taken from each item (stored in Tuple.Item2)
         /// OR NULL if no items can be selected</returns>
-        static public Tuple<int, int>[] ConstructSolution(int camelsLoad, int itemsCount, int[] weights, int[] profits)
+        public static Tuple<int, int>[] ConstructSolution(int camelsLoad, int itemsCount, int[] weights, int[] profits)
         {
             //REMOVE THIS LINE BEFORE START CODING
             throw new NotImplementedException();
