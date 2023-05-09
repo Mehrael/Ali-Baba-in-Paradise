772b23d3fe9e4116c7f7438c9b27676d29e06320
Mehrael <mehraelashraf20@gmail.com>
2023-05-10 00:06:33 +0300

    ConstructingSolution -> 20% easy test -> 0% hard test

--- [TEMPLATE]/AliBabaInParadise/AliBabaInParadise.cs
+++ [TEMPLATE]/AliBabaInParadise/AliBabaInParadise.cs
@@ -13,7 +13,9 @@ namespace Problem
     public static class AliBabaInParadise
     {
         #region YOUR CODE IS HERE
+
         #region FUNCTION#1: Calculate the Value
+
         //Your Code is Here:
         //==================
         /// <summary>
@@ -25,25 +27,30 @@ namespace Problem
         /// <param name="weights">weight of each item</param>
         /// <param name="profits">profit of each item</param>
         /// <returns>Max total profit</returns>
+        
+        private static int[,] ExtraStorage;
         public static int SolveValue(int camelsLoad, int itemsCount, int[] weights, int[] profits)
         {
             //REMOVE THIS LINE BEFORE START CODING
             // throw new NotImplementedException();
-            int[,] ExtraStorage = new int[itemsCount + 1, camelsLoad + 1];
-
+            ExtraStorage = new int[itemsCount + 1, camelsLoad + 1];
             for (int i = 1; i <= itemsCount; i++)
                 for (int j = 1; j <= camelsLoad; j++)
                 {
                     ExtraStorage[i, j] = ExtraStorage[i - 1, j];
+
                     if (j >= weights[i - 1])
                         ExtraStorage[i, j] = Math.Max(ExtraStorage[i, j], ExtraStorage[i, j - weights[i - 1]] + profits[i - 1]);
+
                 }
 
             return ExtraStorage[itemsCount, camelsLoad];
         }
+
         #endregion
 
         #region FUNCTION#2: Construct the Solution
+
         //Your Code is Here:
         //==================
         /// <returns>Tuple array of the selected items to get MAX profit (stored in Tuple.Item1) together with the number of instances taken from each item (stored in Tuple.Item2)
@@ -51,8 +58,24 @@ namespace Problem
         public static Tuple<int, int>[] ConstructSolution(int camelsLoad, int itemsCount, int[] weights, int[] profits)
         {
             //REMOVE THIS LINE BEFORE START CODING
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
+            List<Tuple<int, int>> solution = new List<Tuple<int, int>>();
+            
+            int itemIndex = itemsCount, weightIndex = camelsLoad;
+            
+            while (itemIndex > 0 && weightIndex > 0)
+                if (ExtraStorage[itemIndex, weightIndex] != ExtraStorage[itemIndex - 1, weightIndex])
+                {
+                    solution.Add(new Tuple<int, int>(itemIndex , weights[itemIndex - 1]));
+                    weightIndex -= weights[itemIndex - 1];
+                }
+                else
+                    itemIndex--;
+
+            return solution.ToArray();
         }
+
         #endregion
 
         #endregion
